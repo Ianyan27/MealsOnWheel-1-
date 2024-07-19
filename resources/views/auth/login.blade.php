d55436fc4bc705a6d7e4f9aff48c7b2f5ced353d
Ianyan27 <gerome5252005@gmail.com>
2024-07-19 12:17:28 +0800

    commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -28,21 +28,25 @@
             <div class="block mt-4">
                 <label for="remember_me" class="flex items-center">
                     <x-checkbox id="remember_me" name="remember" />
-                    <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
                 </label>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-button class="ms-4">
+            <div class="flex items-center justify-center mt-4">
+                <x-button class="ml-4">
                     {{ __('Log in') }}
                 </x-button>
             </div>
         </form>
+        <div class="flex items-center justify-center mt-4">
+            @if (Route::has('password.request'))
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mx-2" href="{{ route('password.request') }}">
+                    {{ __('Forgot your password?') }}
+                </a>
+            @endif
+            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mx-2" href="{{ route('register') }}">
+                {{ __('Register') }}
+            </a>
+        </div>
     </x-authentication-card>
 </x-guest-layout>
